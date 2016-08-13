939f382cbcd6669f6d8ceb577c8f3072cd4f8c9d
yeonghoey <yeonghoey@gmail.com>
2016-08-13 22:12:09 +0900

    Working

--- calc.py
+++ calc.py
@@ -2,45 +2,46 @@ import re
 import sys
 
 
-def _(stream):
-    c, t = dict(_=None), dict(_=(lambda _: re.findall('([\d.]+|[-+*/()])', _))(stream))
-    return (lambda: c['_'], lambda: c.update({'_':'_'}) if not t['_'] else (c.update({'_': t['_'][0]}), t.update({'_': (t['_'][1:])})))
+c, t = dict(_=None), dict(_=(lambda _: re.findall('([\d.]+|[-+*/()])', _))(sys.argv[1]))
+_, __ = (lambda: c['_'], lambda: c.update({'_':'_'}) if not t['_'] else (c.update({'_': t['_'][0]}), t.update({'_': (t['_'][1:])})))
+__()
+
+def expr():
+    r = term()
+    return repeat_expr(r)
+
+def repeat_expr(r):
+    global _, __
+    l = _()
+    a = list(map(lambda _: (__(), eval(str(r)+_+str(term()))), filter(lambda _: _ == l, '+-')))
+    return repeat_expr(sum(map(lambda _: _[1], a))) if any(a) else r
 
 
-def expr(g, n):
-    r = term(g, n)
-    return repeat_expr(g, n, r)
-
-def repeat_expr(g, n, r):
-    l = g()
-    a = list(map(lambda _: (n(), eval(str(r)+_+str(term(g, n)))), filter(lambda _: _ == l, '+-')))
-    return repeat_expr(g, n, sum(map(lambda _: _[1], a))) if any(a) else r
+def term():
+    global _, __
+    r = factor()
+    return repeat_term(r)
 
 
-def term(g, n):
-    r = factor(g, n)
-    return repeat_term(g, n, r)
+def repeat_term(r):
+    global _, __
+    l = _()
+    a = list(map(lambda _: (__(), eval(str(r)+_+str(factor()))), filter(lambda _: _ == l, '*/')))
+    return repeat_term(sum(map(lambda _: _[1], a))) if any(a) else r
 
 
-def repeat_term(g, n, r):
-    l = g()
-    a = list(map(lambda _: (n(), eval(str(r)+_+str(factor(g, n)))), filter(lambda _: _ == l, '*/')))
-    return repeat_term(g, n, sum(map(lambda _: _[1], a))) if any(a) else r
-
-
-def factor(g, n):
-    l = g()
-    a = list(map(lambda _: (n(), eval(_+str(factor(g, n)))), filter(lambda _: _ == l, '+-')))
+def factor():
+    global _, __
+    l = _()
+    a = list(map(lambda _: (__(), eval(_+str(factor()))), filter(lambda _: _ == l, '+-')))
     if a: return sum(map(lambda _: _[1], a))
 
-    f = lambda _: (n(), float(_.group(0))) if _ else (_, _)
+    f = lambda _: (__(), float(_.group(0))) if _ else (_, _)
     b, c = f(re.match(r'[\d.]+', l))
     if c: return c
 
-    return (n(), expr(g, n), (None() if g() != ')' else n()))[1] if l == '(' else None()
+    return (__(), expr(), (None() if _() != ')' else __()))[1] if l == '(' else None()
 
 
-_, __ = _(sys.argv[1])
-__()
-result = expr(_, __)
+result = expr()
 print(result) if _() == '_' else None()
