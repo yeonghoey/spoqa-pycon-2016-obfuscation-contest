10006d678e1d1df2eabc4666aab937533085db63
yeonghoey <yeonghoey@gmail.com>
2016-08-13 23:36:05 +0900

    Done

--- calc.py
+++ calc.py
@@ -1,20 +1,13 @@
-import re
-import sys
-
-
-def ___(______, _______, ________): _____ = list(map(lambda _: _[1], list(map(lambda _: (globals()['__'](), eval(str(______)+_+str(eval(_______)))), list(filter(lambda __: __ == globals()['_'](), ________)))))); return ___(sum(_____), _______, ________) if _____ else ______
-
+def ___(______, _______, ________):
+ _____=list(map(lambda _:_[1],list(map(lambda _:(globals()['__'](),eval(str(______)+_+str(eval(_______)))),list(filter(lambda __:__==globals()['_'](), ________))))))
+ return ___(sum(_____),_______,________) if _____ else ______
 def ____():
-    _______ = globals()['_']()
-    _____ = list(map(lambda _: (globals()['__'](), eval(_+str(____()))), filter(lambda _: _ == _______, '+-')))
-    if _____: return sum(map(lambda _: _[1], _____))
-    _________, ________ = (lambda _: (globals()['__'](), float(_.group(0))) if _ else (_, _))(re.match(r'[\d.]+', _______))
-    if ________: return ________
-
-    return (globals()['__'](), ___(___(____(), '____()', '*/'), '___(____(), "____()", "*/")', '+-'), (None() if globals()['_']() != ')' else globals()['__']()))[1] if _______ == '(' else None()
-
-
-_________, __________ = dict(_=None), dict(_=(lambda _: re.findall('([\d.]+|[-+*/()])', _))(sys.argv[1]))
-_, __ = (lambda: _________['_'], lambda: _________.update({'_':'_'}) if not __________['_'] else (_________.update({'_': __________['_'][0]}), __________.update({'_': (__________['_'][1:])})))
+ _______=globals()['_']();_____=list(map(lambda _:(globals()['__'](),eval(_+str(____()))),filter(lambda _:_==_______,'+-')))
+ if _____:return sum(map(lambda _:_[1],_____))
+ _________,________=(lambda _:(globals()['__'](),float(_.group(0)))if _ else (_,_))(__import__('re').match(r'[\d.]+', _______))
+ if ________:return ________
+ return(globals()['__'](),___(___(____(),'____()','*/'),'___(____(), "____()","*/")','+-'),(None() if globals()['_']()!=')'else globals()['__']()))[1]if _______=='(' else None()
+_________,__________=dict(_=None),dict(_=(lambda _:__import__('re').findall('([\d.]+|[-+*/()])',_))(__import__('sys').argv[1]))
+_,__=(lambda:_________['_'],lambda:_________.update({'_':'_'})if not __________['_'] else (_________.update({'_': __________['_'][0]}),__________.update({'_':(__________['_'][1:])})))
 __(); ______ = ___(___(____(), '____()', '*/'),  '___(____(), "____()", "*/")', '+-')
-print(______) if _()=='_' else None()
+print(______)if _()=='_' else None()
