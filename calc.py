703d865bb0c3087b0e1ae2391a839195a02ee8e0
yeonghoey <yeonghoey@gmail.com>
2016-08-13 23:21:54 +0900

    l =7

--- calc.py
+++ calc.py
@@ -2,21 +2,18 @@ import re
 import sys
 
 
-def ___(______, _______, ________):
-    _____ = list(map(lambda _: _[1], list(map(lambda _: (globals()['__'](), eval(str(______)+_+str(eval(_______)))), list(filter(lambda __: __ == globals()['_'](), ________))))))
-    return ___(sum(_____), _______, ________) if _____ else ______
-
+def ___(______, _______, ________): _____ = list(map(lambda _: _[1], list(map(lambda _: (globals()['__'](), eval(str(______)+_+str(eval(_______)))), list(filter(lambda __: __ == globals()['_'](), ________)))))); return ___(sum(_____), _______, ________) if _____ else ______
 
 def ____():
-    l = globals()['_']()
-    _____ = list(map(lambda _: (globals()['__'](), eval(_+str(____()))), filter(lambda _: _ == l, '+-')))
+    _______ = globals()['_']()
+    _____ = list(map(lambda _: (globals()['__'](), eval(_+str(____()))), filter(lambda _: _ == _______, '+-')))
     if _____: return sum(map(lambda _: _[1], _____))
 
     f = lambda _: (globals()['__'](), float(_.group(0))) if _ else (_, _)
-    b, c = f(re.match(r'[\d.]+', l))
+    b, c = f(re.match(r'[\d.]+', _______))
     if c: return c
 
-    return (globals()['__'](), ___(___(____(), '____()', '*/'), '___(____(), "____()", "*/")', '+-'), (None() if globals()['_']() != ')' else globals()['__']()))[1] if l == '(' else None()
+    return (globals()['__'](), ___(___(____(), '____()', '*/'), '___(____(), "____()", "*/")', '+-'), (None() if globals()['_']() != ')' else globals()['__']()))[1] if _______ == '(' else None()
 
 
 c, t = dict(_=None), dict(_=(lambda _: re.findall('([\d.]+|[-+*/()])', _))(sys.argv[1]))
